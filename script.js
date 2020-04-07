4b2c6ad6c89406726ce6ede4ee5f96402513443c
Saurabh Datta <hi@dattasaurabh.com>
2020-04-07 20:36:20 +0800

    adding max travel attribute

--- script.js
+++ script.js
@@ -1,134 +1,141 @@
-  "use strict";
+    "use strict";
 
-  let port;
-  let reader;
-  let inputDone;
-  let outputDone;
-  let inputStream;
-  let outputStream;
-  let currCMD;
+    let port;
+    let reader;
+    let inputDone;
+    let outputDone;
+    let inputStream;
+    let outputStream;
+    let currCMD;
 
-  const log = document.getElementById("log");
-  const butConnect = document.getElementById("butConnect");
-  const butUnlock = document.getElementById("butUnlock");
-  const butHome = document.getElementById("butHome");
-  const butClearGRBLLog = document.getElementById("butClearGRBLLog");
+    const log = document.getElementById("log");
+    const butConnect = document.getElementById("butConnect");
+    const butUnlock = document.getElementById("butUnlock");
+    const butHome = document.getElementById("butHome");
+    const butClearGRBLLog = document.getElementById("butClearGRBLLog");
 
-  const frate = document.getElementById("frate");
-  const setFeedRate = document.getElementById("setFeedRate");
+    const frate = document.getElementById("frate");
+    const setFeedRate = document.getElementById("setFeedRate");
 
-  const typedCMD = document.getElementById("cmdSection");
-  const sendTypedCMD = document.getElementById("sendTypedCMD");
+    const fbedlength = document.getElementById("fbedlength");
+    const setBedLength = document.getElementById("setBedLength");
 
-  const lastCMD = document.getElementById("lastCMD");
+    const typedCMD = document.getElementById("cmdSection");
+    const sendTypedCMD = document.getElementById("sendTypedCMD");
 
-  document.addEventListener("DOMContentLoaded", () => {
-    butConnect.addEventListener("click", clickConnect);
-    butUnlock.addEventListener("click", sendUnLockCMD);
-    butHome.addEventListener("click", sendHomeCMD);
+    const lastCMD = document.getElementById("lastCMD");
 
-    frate.addEventListener("keyup", sendSetFeedRateCommandUponENTER);
-    setFeedRate.addEventListener("click", sendSetFeedRateCommand);
+    document.addEventListener("DOMContentLoaded", () => {
+      butConnect.addEventListener("click", clickConnect);
+      butUnlock.addEventListener("click", sendUnLockCMD);
+      butHome.addEventListener("click", sendHomeCMD);
 
-    typedCMD.addEventListener("keyup", sendTypedCommandUponENTER);
-    sendTypedCMD.addEventListener("click", sendTypedCommand);
+      frate.addEventListener("keyup", sendSetFeedRateCommandUponENTER);
+      setFeedRate.addEventListener("click", sendSetFeedRateCommand);
 
-    butClearGRBLLog.addEventListener("click", clearGRBLlogArea);
 
-    if ("serial" in navigator) {
-      console.log("Serial is supported");
-      const notSupported = document.getElementById("notSupported");
-      notSupported.classList.add("hidden");
-    }
-  });
+      fbedlength.addEventListener("keyup", sendSetBedlengthCommandUponENTER);
+      setBedLength.addEventListener("click", sendBedlengthCommand);
 
-  /**
-   * --- connect
-   * Opens a Web Serial connection and sets up the input and
-   * output stream.
-   */
-  async function connect() {
-    // - Request a port and open a connection.
-    port = await navigator.serial.requestPort();
-    // - Wait for the port to open.
-    await port.open({ baudrate: 115200 });
+      typedCMD.addEventListener("keyup", sendTypedCommandUponENTER);
+      sendTypedCMD.addEventListener("click", sendTypedCommand);
 
-    //Setup the output stream here.
-    const encoder = new TextEncoderStream();
-    outputDone = encoder.readable.pipeTo(port.writable);
-    outputStream = encoder.writable;
+      butClearGRBLLog.addEventListener("click", clearGRBLlogArea);
 
-    //To read the stream here.
-    let decoder = new TextDecoderStream();
-    inputDone = port.readable.pipeTo(decoder.writable);
-    inputStream = decoder.readable;
-    reader = inputStream.getReader();
-    readLoop();
-  }
-
-  /**
-   * --- clickConnect
-   * Click handler for the connect/disconnect button.
-   */
-  async function clickConnect() {
-    if (port) {
-      await disconnect();
-      toggleUIConnected(false);
-      return;
-    }
-
-    await connect();
-    toggleUIConnected(true);
-  }
-
-  /**
-   * --- disconnect
-   * Closes the Web Serial connection.
-   */
-  async function disconnect() {
-    if (reader) {
-      await reader.cancel();
-      await inputDone.catch(() => {});
-      reader = null;
-      inputDone = null;
-    }
-
-    if (outputStream) {
-      await outputStream.getWriter().close();
-      await outputDone;
-      outputStream = null;
-      outputDone = null;
-    }
-
-    await port.close();
-    port = null;
-  }
-
-  function toggleUIConnected(connected) {
-    let lbl = "CONNECT SERIAL";
-    if (connected) {
-      lbl = "DIS-CONNECT SERIAL";
-    }
-    butConnect.textContent = lbl;
-  }
-
-  /**
-   * @name readLoop
-   * Reads data from the input stream and displays it on screen.
-   */
-  async function readLoop() {
-    while (true) {
-      const { value, done } = await reader.read();
-      if (value) {
-        log.textContent += value + "\n";
+      if ("serial" in navigator) {
+        console.log("Serial is supported");
+        const notSupported = document.getElementById("notSupported");
+        notSupported.classList.add("hidden");
       }
-      if (done) {
-        console.log("[readLoop] DONE", done);
-        reader.releaseLock();
-        break;
+    });
+
+    /**
+     * --- connect
+     * Opens a Web Serial connection and sets up the input and
+     * output stream.
+     */
+    async function connect() {
+      // - Request a port and open a connection.
+      port = await navigator.serial.requestPort();
+      // - Wait for the port to open.
+      await port.open({ baudrate: 115200 });
+
+      //Setup the output stream here.
+      const encoder = new TextEncoderStream();
+      outputDone = encoder.readable.pipeTo(port.writable);
+      outputStream = encoder.writable;
+
+      //To read the stream here.
+      let decoder = new TextDecoderStream();
+      inputDone = port.readable.pipeTo(decoder.writable);
+      inputStream = decoder.readable;
+      reader = inputStream.getReader();
+      readLoop();
+    }
+
+    /**
+     * --- clickConnect
+     * Click handler for the connect/disconnect button.
+     */
+    async function clickConnect() {
+      if (port) {
+        await disconnect();
+        toggleUIConnected(false);
+        return;
+      }
+
+      await connect();
+      toggleUIConnected(true);
+    }
+
+    /**
+     * --- disconnect
+     * Closes the Web Serial connection.
+     */
+    async function disconnect() {
+      if (reader) {
+        await reader.cancel();
+        await inputDone.catch(() => {});
+        reader = null;
+        inputDone = null;
+      }
+
+      if (outputStream) {
+        await outputStream.getWriter().close();
+        await outputDone;
+        outputStream = null;
+        outputDone = null;
+      }
+
+      await port.close();
+      port = null;
+    }
+
+    function toggleUIConnected(connected) {
+      let lbl = "CONNECT SERIAL";
+      if (connected) {
+        lbl = "DIS-CONNECT SERIAL";
+      }
+      butConnect.textContent = lbl;
+    }
+
+    /**
+     * @name readLoop
+     * Reads data from the input stream and displays it on screen.
+     */
+    async function readLoop() {
+      while (true) {
+        const { value, done } = await reader.read();
+        if (value) {
+          log.textContent += value + "\n";
+        }
+        if (done) {
+          console.log("[readLoop] DONE", done);
+          reader.releaseLock();
+          break;
+        }
       }
     }
-  }
 
   /**
    * --- writeToStream
@@ -186,14 +193,31 @@
     }
   }
 
-function showElaborateCurrCMD(txt, cmd){
-  var desc = "--> " + String(txt) + ' ' + String(cmd);
-  console.log(desc);
-  log.textContent += desc + "\n";
-}
+  function sendBedlengthCommand(){
+    var cmd_elems = ['$132=','']
+    cmd_elems[1] = fbedlength.value;
+    var setBLcmd = cmd_elems.join('');
+    // console.log(setFRcmd);
+    currCMD = setBLcmd;
+    showElaborateCurrCMD("SENDING SET FEED-RATE CMD:", currCMD);
+    writeToStream(currCMD);
+  }
 
-function clearGRBLlogArea(){
-  // CLEAR the log area
-  log.innerHTML = "";
-  console.log("clear log area");
-}
+  function sendSetBedlengthCommandUponENTER(ev){
+    if (ev.keyCode == 13) {
+        sendBedlengthCommand();
+      }
+  }
+
+
+  function showElaborateCurrCMD(txt, cmd){
+    var desc = "--> " + String(txt) + ' ' + String(cmd);
+    console.log(desc);
+    log.textContent += desc + "\n";
+  }
+
+  function clearGRBLlogArea(){
+    // CLEAR the log area
+    log.innerHTML = "";
+    console.log("clear log area");
+  }
